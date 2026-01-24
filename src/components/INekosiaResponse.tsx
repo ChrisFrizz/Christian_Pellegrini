418872f782e861802b30ed31e3fc1a165b0a4e46
Christian Pellegrini <christianpellegrini16@gmail.com>
2026-01-24 12:34:52 +0100

    300 righe di bottoni... ma almeno funziona

--- src/components/INekosiaResponse.tsx
+++ src/components/INekosiaResponse.tsx
@@ -7,9 +7,3 @@ export interface INekosiaResponse {
     compressed: { url: string; extension: string };
   };
 }
-
-export const POPULAR_TAGS = [
-  "catgirl", "foxgirl", "maid", "waifu", "neko", "cute", 
-  "animal-ears", "cat-ears", "white-hair", "thigh-high-socks",
-  "vtuber", "tail-with-ribbon", "school-uniform", "smile"
-] as const;
--- src/pages/PopularTags.tsx
+++ src/pages/PopularTags.tsx
@@ -2,14 +2,14 @@ import { useQuery } from "@tanstack/react-query"
 import type { INekosiaResponse } from "../components/INekosiaResponse";
 import { useState } from "react";
 import LikeButton from "../components/LikeButton";
+import axios from "axios";
 
 export default function PopularTags() {
-    
+
     const [enabled, setEnabled] = useState(false);
     const [imageURL, setImageURL] = useState("");
     const [imageURLCompressed, setImageURLCompressed] = useState("");
     const [highQuality, setHighQuality] = useState(false);
-    const [category, setCategory] = useState("");
 
     const { data, isLoading, error } = useQuery<INekosiaResponse>({
         enabled: enabled,
@@ -18,6 +18,17 @@ export default function PopularTags() {
             .then(data => data.json())
     });
 
+    async function fetchImage(value: string) {
+        try {
+            const res = await axios.get(value);
+            setImageURL(res.data.image.original.url);
+            setImageURLCompressed(res.data.image.compressed.url);
+            setHighQuality(false);
+            console.log(setImageURLCompressed);
+        } catch (err) {
+            console.error("Error fetching image:", err);
+        }
+    }
 
     if (isLoading) return <h3>Caricamento in corso</h3>
     if (error) return <h3>Si è verificato un errore: {error.message}</h3>
@@ -25,186 +36,242 @@ export default function PopularTags() {
     return (
         <>
             <h1>Here are the most popular tags:</h1>
-            <button style={{marginRight:'10px'}}
-            
-            onClick={() => {
-                setEnabled(true);
-                if (data) {
-                    setImageURLCompressed(data.image.compressed.url);
-                    setImageURL(data.image.original.url);
-                    setHighQuality(false);
-                }
-            }}>
+            <button style={{ marginRight: '10px' }}
+
+                onClick={() => {
+                    setEnabled(true);
+                    if (data) {
+                        setImageURLCompressed(data.image.compressed.url);
+                        setImageURL(data.image.original.url);
+                        setHighQuality(false);
+                    }
+                    setEnabled(false);
+                }}>
                 catgirl
             </button>
 
             <button
-                style={{marginRight:'10px', marginTop:'10px'}}
+                style={{ marginRight: '10px', marginTop: '10px' }}
                 onClick={() => {
-                setEnabled(true);
-                if (data) {
-                    setImageURLCompressed(data.image.compressed.url);
-                    setImageURL(data.image.original.url);
-                    setHighQuality(false);
-                }
-            }}>
+                    fetchImage('https://api.nekosia.cat/api/v1/images/foxgirl')
+                }}>
                 foxgirl
             </button>
 
-            <button 
-            style={{marginRight:'10px', marginTop:'10px'}}
-            onClick={() => {
-                setEnabled(true);
-                if (data) {
-                    setImageURLCompressed(data.image.compressed.url);
-                    setImageURL(data.image.original.url);
-                    setHighQuality(false);
-                }
-            }}>
-                maid
+            <button
+                style={{ marginRight: '10px', marginTop: '10px' }}
+                onClick={() => {
+                    fetchImage('https://api.nekosia.cat/api/v1/images/wolfgirl')
+                }}>
+                wolfgirl
             </button>
 
-            <button 
-            style={{marginRight:'10px', marginTop:'10px'}}
-            onClick={() => {
-                setEnabled(true);
-                if (data) {
-                    setImageURLCompressed(data.image.compressed.url);
-                    setImageURL(data.image.original.url);
-                    setHighQuality(false);
-                }
-            }}>
-                waifu
-            </button>
-
-            <button 
-            style={{marginRight:'10px', marginTop:'10px'}}
-            onClick={() => {
-                setEnabled(true);
-                if (data) {
-                    setImageURLCompressed(data.image.compressed.url);
-                    setImageURL(data.image.original.url);
-                    setHighQuality(false);
-                }
-            }}>
-                neko
-            </button>
-
-            <button 
-            style={{marginRight:'10px', marginTop:'10px'}}
-            onClick={() => {
-                setEnabled(true);
-                if (data) {
-                    setImageURLCompressed(data.image.compressed.url);
-                    setImageURL(data.image.original.url);
-                    setHighQuality(false);
-                }
-            }}>
-                cute
-            </button>
-
-            <button 
-            style={{marginRight:'10px', marginTop:'10px'}}
-            onClick={() => {
-                setEnabled(true);
-                if (data) {
-                    setImageURLCompressed(data.image.compressed.url);
-                    setImageURL(data.image.original.url);
-                    setHighQuality(false);
-                }
-            }}>
+            <button
+                style={{ marginRight: '10px', marginTop: '10px' }}
+                onClick={() => {
+                    fetchImage('https://api.nekosia.cat/api/v1/images/animal-ears')
+                }}>
                 animal ears
             </button>
 
-            <button 
-            style={{marginRight:'10px', marginTop:'10px'}}
-            onClick={() => {
-                setEnabled(true);
-                if (data) {
-                    setImageURLCompressed(data.image.compressed.url);
-                    setImageURL(data.image.original.url);
-                    setHighQuality(false);
-                }
-            }}>
-                cat ears
+            <button
+                style={{ marginRight: '10px', marginTop: '10px' }}
+                onClick={() => {
+                    fetchImage('https://api.nekosia.cat/api/v1/images/tail')
+                }}>
+                tail
             </button>
 
-            <button 
-            style={{marginRight:'10px', marginTop:'10px'}}
-            onClick={() => {
-                setEnabled(true);
-                if (data) {
-                    setImageURLCompressed(data.image.compressed.url);
-                    setImageURL(data.image.original.url);
-                    setHighQuality(false);
-                }
-            }}>
-                white hair
-            </button>
-
-            <button 
-            style={{marginRight:'10px', marginTop:'10px'}}
-            onClick={() => {
-                setEnabled(true);
-                if (data) {
-                    setImageURLCompressed(data.image.compressed.url);
-                    setImageURL(data.image.original.url);
-                    setHighQuality(false);
-                }
-            }}>
-                thigh-high socks
-            </button>
-
-            <button 
-            style={{marginRight:'10px', marginTop:'10px'}}
-            onClick={() => {
-                setEnabled(true);
-                if (data) {
-                    setImageURLCompressed(data.image.compressed.url);
-                    setImageURL(data.image.original.url);
-                    setHighQuality(false);
-                }
-            }}>
-                v-tuber
-            </button>
-
-            <button 
-            style={{marginRight:'10px', marginTop:'10px'}}
-            onClick={() => {
-                setEnabled(true);
-                if (data) {
-                    setImageURLCompressed(data.image.compressed.url);
-                    setImageURL(data.image.original.url);
-                    setHighQuality(false);
-                }
-            }}>
+            <button
+                style={{ marginRight: '10px', marginTop: '10px' }}
+                onClick={() => {
+                    fetchImage('https://api.nekosia.cat/api/v1/images/tail-with-ribbon')
+                }}>
                 tail with ribbon
             </button>
 
-            <button 
-            style={{marginRight:'10px', marginTop:'10px'}}
-            onClick={() => {
-                setEnabled(true);
-                if (data) {
-                    setImageURLCompressed(data.image.compressed.url);
-                    setImageURL(data.image.original.url);
-                    setHighQuality(false);
-                }
-            }}>
-                school uniform
+            <button
+                style={{ marginRight: '10px', marginTop: '10px' }}
+                onClick={() => {
+                    fetchImage('https://api.nekosia.cat/api/v1/images/tail-from-under-skirt')
+                }}>
+                tail from under-skirt
             </button>
 
-            <button 
-            style={{marginRight:'10px', marginTop:'10px'}}
-            onClick={() => {
-                setEnabled(true);
-                if (data) {
-                    setImageURLCompressed(data.image.compressed.url);
-                    setImageURL(data.image.original.url);
-                    setHighQuality(false);
-                }
-            }}>
-                smile
+            <button
+                style={{ marginRight: '10px', marginTop: '10px' }}
+                onClick={() => {
+                    fetchImage('https://api.nekosia.cat/api/v1/images/cute')
+                }}>
+                cute
+            </button>
+
+            <button
+                style={{ marginRight: '10px', marginTop: '10px' }}
+                onClick={() => {
+                    fetchImage('https://api.nekosia.cat/api/v1/images/blue-archive')
+                }}>
+                blue archive
+            </button>
+
+            <button
+                style={{ marginRight: '10px', marginTop: '10px' }}
+                onClick={() => {
+                    fetchImage('https://api.nekosia.cat/api/v1/images/maid')
+                }}>
+                maid
+            </button>
+
+            <button
+                style={{ marginRight: '10px', marginTop: '10px' }}
+                onClick={() => {
+                    fetchImage('https://api.nekosia.cat/api/v1/images/vtuber')
+                }}>
+                v-tuber
+            </button>
+
+            <button
+                style={{ marginRight: '10px', marginTop: '10px' }}
+                onClick={() => {
+                    fetchImage('https://api.nekosia.cat/api/v1/images/w-sitting')
+                }}>
+                w-sitting
+            </button>
+
+            <button
+                style={{ marginRight: '10px', marginTop: '10px' }}
+                onClick={() => {
+                    fetchImage('https://api.nekosia.cat/api/v1/images/lying-down')
+                }}>
+                lying down
+            </button>
+
+            <button
+                style={{ marginRight: '10px', marginTop: '10px' }}
+                onClick={() => {
+                    fetchImage('https://api.nekosia.cat/api/v1/images/hands-forming-a-heart')
+                }}>
+                heart hands
+            </button>
+
+            <button
+                style={{ marginRight: '10px', marginTop: '10px' }}
+                onClick={() => {
+                    fetchImage('https://api.nekosia.cat/api/v1/images/wink')
+                }}>
+                wink
+            </button>
+
+            <button
+                style={{ marginRight: '10px', marginTop: '10px' }}
+                onClick={() => {
+                    fetchImage('https://api.nekosia.cat/api/v1/images/thigh-high-socks')
+                }}>
+                thigh-high socks
+            </button>
+
+            <button
+                style={{ marginRight: '10px', marginTop: '10px' }}
+                onClick={() => {
+                    fetchImage('https://api.nekosia.cat/api/v1/images/knee-high-socks')
+                }}>
+                knee-high socks
+            </button>
+
+            <button
+                style={{ marginRight: '10px', marginTop: '10px' }}
+                onClick={() => {
+                    fetchImage('https://api.nekosia.cat/api/v1/images/white-thigh')
+                }}>
+                white tighs
+            </button>
+
+            <button
+                style={{ marginRight: '10px', marginTop: '10px' }}
+                onClick={() => {
+                    fetchImage('https://api.nekosia.cat/api/v1/images/black-thigh')
+                }}>
+                black tighs
+            </button>
+
+            <button
+                style={{ marginRight: '10px', marginTop: '10px' }}
+                onClick={() => {
+                    fetchImage('https://api.nekosia.cat/api/v1/images/heterochromia')
+                }}>
+                homophobia💛💙
+            </button>
+
+            <button
+                style={{ marginRight: '10px', marginTop: '10px' }}
+                onClick={() => {
+                    fetchImage('https://api.nekosia.cat/api/v1/images/uniform')
+                }}>
+                uniform
+            </button>
+
+            <button
+                style={{ marginRight: '10px', marginTop: '10px' }}
+                onClick={() => {
+                    fetchImage('https://api.nekosia.cat/api/v1/images/hoodie')
+                }}>
+                hoodie
+            </button>
+
+            <button
+                style={{ marginRight: '10px', marginTop: '10px' }}
+                onClick={() => {
+                    fetchImage('https://api.nekosia.cat/api/v1/images/ribbon')
+                }}>
+                ribbon
+            </button>
+
+            <button
+                style={{ marginRight: '10px', marginTop: '10px' }}
+                onClick={() => {
+                    fetchImage('https://api.nekosia.cat/api/v1/images/white-hair')
+                }}>
+                white hair
+            </button>
+
+            <button
+                style={{ marginRight: '10px', marginTop: '10px' }}
+                onClick={() => {
+                    fetchImage('https://api.nekosia.cat/api/v1/images/blue-hair')
+                }}>
+                blue hair
+            </button>
+
+            <button
+                style={{ marginRight: '10px', marginTop: '10px' }}
+                onClick={() => {
+                    fetchImage('https://api.nekosia.cat/api/v1/images/long-hair')
+                }}>
+                long hair
+            </button>
+
+            <button
+                style={{ marginRight: '10px', marginTop: '10px' }}
+                onClick={() => {
+                    fetchImage('https://api.nekosia.cat/api/v1/images/blonde')
+                }}>
+                blonde
+            </button>
+
+            <button
+                style={{ marginRight: '10px', marginTop: '10px' }}
+                onClick={() => {
+                    fetchImage('https://api.nekosia.cat/api/v1/images/blue-eyes')
+                }}>
+                blue eyes
+            </button>
+
+            <button
+                style={{ marginRight: '10px', marginTop: '10px' }}
+                onClick={() => {
+                    fetchImage('https://api.nekosia.cat/api/v1/images/purple-eyes')
+                }}>
+                purple eyes
             </button>
 
             {imageURLCompressed && !highQuality && (
@@ -246,7 +313,5 @@ export default function PopularTags() {
                 </>
             )}
         </>
-
-
     )
 }
